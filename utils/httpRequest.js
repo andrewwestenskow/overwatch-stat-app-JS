17a569266cb30f80d3043f0d4a3aba5d3a46cfbf
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-07-14 17:52:07 -0600

    Comments

--- utils/httpRequest.js
+++ utils/httpRequest.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
 
 export default ({method, url, data}) => {
+  //TODO Replace this later
   const fullUrl = 'http://10.0.2.2:3050/api' + url;
   const authToken = AsyncStorage.getItem('token');
 
