5e6f031015063037654445d60f5e23f1dfec9c36
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-08-05 16:44:37 -0600

    Footer

--- components/wizard/WizardHeader.js
+++ components/wizard/WizardHeader.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import VictoryDefeat from './VictoryDefeat';
 
 const WizardHeader = props => {
   const {
     reducer: {dispatch, actions, match},
+    maps,
+    heroes,
   } = props;
+
+  const [map, setMap] = useState({});
+
+  useEffect(() => {
+    if (match.map_id) {
+      const newMap = maps.find(e => e.id === match.map_id);
+      setMap(newMap);
+    }
+  });
+
+  console.log(map);
+
   return (
     <View style={styles.sectionHeader}>
+      <Text>Match Summary: </Text>
       <VictoryDefeat reducer={{dispatch, actions}} />
-      {match.map_id && <Text>Map: {match.map_id}</Text>}
+      <View style={styles.headerRow}>
+        {match.map_id && (
+          <View style={styles.headerRowItem}>
+            <Text>Map: </Text>
+            <Image style={styles.headerRowImage} source={{uri: map.image}} />
+          </View>
+        )}
+      </View>
     </View>
   );
 };
@@ -19,5 +41,21 @@ export default WizardHeader;
 const styles = StyleSheet.create({
   sectionHeader: {
     padding: 15,
+  },
+  headerRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingTop: 10,
+  },
+  headerRowItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '50%',
+  },
+  headerRowImage: {
+    height: 50,
+    width: '80%',
   },
 });
